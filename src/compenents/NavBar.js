93ec4a6d7c2c52c2aeac28c8ad755247783a662e
rsenne <rsenne@student.wethinkcode.co.za>
2022-04-13 14:50:09 +0200

    add NavBar

---
+++ src/compenents/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuctioneerBtn from './AuctioneerBtn'           
+import logo from '../logo.png'
+
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
